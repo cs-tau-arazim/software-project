19418f36b1ab4f8db7e21ec30242d5b7915e5939
galmw <Gal@Gals-MacBook-Pro.local>
2016-05-24 15:13:05 +0300

    finished splogger_unit_test

--- SPAssignment3/SPLogger.c
+++ SPAssignment3/SPLogger.c
@@ -23,21 +23,7 @@ struct sp_logger_t {
 	SP_LOGGER_LEVEL level; //Indicates the level
 };
 
-/**
- * Creates a logger. This function should be called once, prior
- * to the usage of any SP Logger print functions. It is the responsibility
- * of the user to create the logger prior to usage, and the logger
- * must be destroyed at the end of usage.
- *
- * @param filename - The name of the log file, if not specified stdout is used
- * 					 as default.
- * @param level - The level of the logger prints
- * @return
- * SP_LOGGER_DEFINED 			- The logger has been defined
- * SP_LOGGER_OUT_OF_MEMORY 		- In case of memory allocation failure
- * SP_LOGGER_CANNOT_OPEN_FILE 	- If the file given by filename cannot be opened
- * SP_LOGGER_SUCCESS 			- In case the logger has been successfully opened
- */
+
 SP_LOGGER_MSG spLoggerCreate(const char* filename, SP_LOGGER_LEVEL level) {
 	if (logger != NULL) { //Already defined
 		return SP_LOGGER_DEFINED;
@@ -62,10 +48,6 @@ SP_LOGGER_MSG spLoggerCreate(const char* filename, SP_LOGGER_LEVEL level) {
 	return SP_LOGGER_SUCCESS;
 }
 
-/**
- * Frees all memory allocated for the logger. If the logger is not defined
- * then nothing happens.
- */
 void spLoggerDestroy() {
 	if (!logger) {
 		return;
@@ -77,41 +59,6 @@ void spLoggerDestroy() {
 	logger = NULL;
 }
 
-/**
- * 	Prints error message. The error message format is given below:
- * 	---ERROR---
- * 	- file: <file>
- *  - function: <function>
- *  - line: <line>
- *  - message: <msg>
- *
- * 	<file> 	   - is the string given by file, it represents the file in which
- * 		   		 the error print call occurred.
- * 	<function> - is the string given by function, it represents the function in which
- * 			   	 the error print call occurred.
- * 	<line> 	   - is the string given by line, it represents the line in which
- * 		   		 the error print call occurred
- * 	<msg> 	   - is the string given by msg, it contains the msg given by the user
- *
- * 	Error messages will be printed at levels:
- *
- * 	SP_LOGGER_ERROR_LEVEL,
- *	SP_LOGGER_WARNING_ERROR_LEVEL,
- *	SP_LOGGER_INFO_WARNING_ERROR_LEVEL,
- *	SP_LOGGER_DEBUG_INFO_WARNING_ERROR_LEVEL
- *
- * 	A new line will be printed after the print call.
- *
- * @param msg     	- The message to printed
- * @param file    	- A string representing the filename in which spLoggerPrintError call occurred
- * @param function 	- A string representing the function name in which spLoggerPrintError call ocurred
- * @param line		- A string representing the line in which the function call occurred
- * @return
- * SP_LOGGER_UNDIFINED 			- If the logger is undefined
- * SP_LOGGER_INVAlID_ARGUMENT	- If any of msg or file or function are null or line is negative
- * SP_LOGGER_WRITE_FAIL			- If Write failure occurred
- * SP_LOGGER_SUCCESS			- otherwise
- */
 SP_LOGGER_MSG spLoggerPrintError(const char* msg, const char* file,
 		const char* function, const int line) {
 	if (logger == NULL)
@@ -124,45 +71,11 @@ SP_LOGGER_MSG spLoggerPrintError(const char* msg, const char* file,
 	if (k < 0 || j < 0)
 		return SP_LOGGER_WRITE_FAIL;
 	// TODO what is write failure?? I think this is it:
-		// "If successful, the total number of characters written is returned
-		// otherwise, a negative number is returned."
+	// "If successful, the total number of characters written is returned
+	// otherwise, a negative number is returned."
 	return SP_LOGGER_SUCCESS;
 }
 
-/**
- * 	Prints warning message. The warning message format is given below:
- * 	---WARNING---
- * 	- file: <file>
- *  - function: <function>
- *  - line: <line>
- *  - message: <msg>
- *
- * 	<file> 	   - is the string given by file, it represents the file in which
- * 		   		 the warning print call occurred.
- * 	<function> - is the string given by function, it represents the function in which
- * 			   	 the warning print call occurred.
- * 	<line> 	   - is the string given by line, it represents the line in which
- * 		   		 the warning print call occurred
- * 	<msg> 	   - is the string given by msg, it contains the msg given by the user
- *
- * 	Warning messages will be printed at levels:
- *
- *	SP_LOGGER_WARNING_ERROR_LEVEL,
- *	SP_LOGGER_INFO_WARNING_ERROR_LEVEL,
- *	SP_LOGGER_DEBUG_INFO_WARNING_ERROR_LEVEL
- *
- *	A new line will be printed after the print call.
- *
- * @param msg     	- The message to printed
- * @param file    	- A string representing the filename in which spLoggerPrintWarning call occurred
- * @param function 	- A string representing the function name in which spLoggerPrintWarning call ocurred
- * @param line		- A string representing the line in which the spLoggerPrintWarning call occurred
- * @return
- * SP_LOGGER_UNDIFINED 			- If the logger is undefined
- * SP_LOGGER_INVAlID_ARGUMENT	- If any of msg or file or function are null or line is negative
- * SP_LOGGER_WRITE_FAIL			- If write failure occurred
- * SP_LOGGER_SUCCESS			- otherwise
- */
 SP_LOGGER_MSG spLoggerPrintWarning(const char* msg, const char* file,
 		const char* function, const int line) {
 	if (logger == NULL)
@@ -180,27 +93,7 @@ SP_LOGGER_MSG spLoggerPrintWarning(const char* msg, const char* file,
 	}
 	return SP_LOGGER_SUCCESS;
 }
-/**
- * 	Prints Info message. The info message format is given below:
- * 	---INFO---
- *  - message: <msg>
- *
- * 	<msg> 	   - is the string given by msg, it contains the msg given by the user
- *
- * 	Info messages will be printed at levels:
- *
- *	SP_LOGGER_INFO_WARNING_ERROR_LEVEL,
- *	SP_LOGGER_DEBUG_INFO_WARNING_ERROR_LEVEL
- *
- * 	A new line will be printed after the print call.
- *
- * @param msg     	- The message to printed
- * @return
- * SP_LOGGER_UNDIFINED 			- If the logger is undefined
- * SP_LOGGER_INVAlID_ARGUMENT	- If msg is null
- * SP_LOGGER_WRITE_FAIL			- If Write failure occurred
- * SP_LOGGER_SUCCESS			- otherwise
- */
+
 SP_LOGGER_MSG spLoggerPrintInfo(const char* msg) {
 	if (logger == NULL)
 		return SP_LOGGER_UNDIFINED;
@@ -217,72 +110,33 @@ SP_LOGGER_MSG spLoggerPrintInfo(const char* msg) {
 	return SP_LOGGER_SUCCESS;
 }
 
-/**
- * 	Prints the debug message. The debug message format is given below:
- * 	---DEBUG---
- * 	- file: <file>
- *  - function: <function>
- *  - line: <line>
- *  - message: <msg>
- *
- * 	<file> 	   - is the string given by file, it represents the file in which
- * 		   		 the debug print call occurred.
- * 	<function> - is the string given by function, it represents the function in which
- * 			   	 the debug print call occurred.
- * 	<line> 	   - is the string given by line, it represents the line in which
- * 		   		 the debug print call occurred
- * 	<msg> 	   - is the string given by msg, it contains the msg given by the user
- *
- * 	Debug messages will be printed at level:
- *
- *	SP_LOGGER_DEBUG_INFO_WARNING_ERROR_LEVEL
- *
- * 	A new line will be printed after the print call.
- *
- * @param msg     	- The message to printed
- * @param file    	- A string representing the filename in which spLoggerPrintWarning call occurred
- * @param function 	- A string representing the function name in which spLoggerPrintWarning call ocurred
- * @param line		- A string representing the line in which the function call occurred
- * @return
- * SP_LOGGER_UNDIFINED 			- If the logger is undefined
- * SP_LOGGER_INVAlID_ARGUMENT	- If any of msg or file or function are null or line is negative
- * SP_LOGGER_WRITE_FAIL			- If Write failure occurred
- * SP_LOGGER_SUCCESS			- otherwise
- */
 SP_LOGGER_MSG spLoggerPrintDebug(const char* msg, const char* file,
 		const char* function, const int line) {
 	if (logger == NULL)
 		return SP_LOGGER_UNDIFINED;
 	if (msg == NULL)
 		return SP_LOGGER_INVAlID_ARGUMENT;
-	// TODO what is write failure??
+
 	if (logger->level == SP_LOGGER_DEBUG_INFO_WARNING_ERROR_LEVEL) {
 		int k = fprintf(logger->outputChannel, DEBUG_LINE);
 		int j = printMsg(msg, file, function, line);
+
+		// check for write failure
 		if (k < 0 || j < 0)
 			return SP_LOGGER_WRITE_FAIL;
 	}
 	return SP_LOGGER_SUCCESS;
 }
 
-/**
- * The given message is printed. A new line is printed at the end of msg
- * The message will be printed in all levels.
- *
- * @param msg - The message to be printed
- * @return
- * SP_LOGGER_UNDIFINED 			- If the logger is undefined
- * SP_LOGGER_INVAlID_ARGUMENT	- If msg is null
- * SP_LOGGER_WRITE_FAIL			- If Write failure occurred
- * SP_LOGGER_SUCCESS			- otherwise
- */
 SP_LOGGER_MSG spLoggerPrintMsg(const char* msg) {
 	if (logger == NULL)
 		return SP_LOGGER_UNDIFINED;
 	if (msg == NULL)
 		return SP_LOGGER_INVAlID_ARGUMENT;
-	// TODO what is write failure??
+
 	int k = fprintf(logger->outputChannel, "- message: %s\n", msg);
+
+	// check for write failure
 	if (k < 0)
 		return SP_LOGGER_WRITE_FAIL;
 	return SP_LOGGER_SUCCESS;
@@ -291,6 +145,10 @@ SP_LOGGER_MSG spLoggerPrintMsg(const char* msg) {
 int printMsg(const char* msg, const char* file, const char* function,
 		const int line) {
 	FILE* f = logger->outputChannel;
-	int k = fprintf(f, "- file: %s\n- function: %s\n- line: %d\n- message: %s\n", file, function, line, msg);
+	int k = fprintf(f,
+			"- file: %s\n- function: %s\n- line: %d\n- message: %s\n", file,
+			function, line, msg);
+
+	// if there was a write failure, will return a negative integer
 	return k;
 }
---
+++ SPAssignment3/unit_tests/sp_logger_unit_test.c
@@ -0,0 +1,185 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "unit_test_util.h" //SUPPORTING MACROS ASSERT_TRUE/ASSERT_FALSE etc..
+#include "../SPLogger.h"
+
+// The following macro receives a function and checks it returns "SP_LOGGER_INVAlID_ARGUMENT"
+// whenever it should according to the documentation-
+// "If any of msg or file or function are null or line is negative".
+#define checkArguments(func) { \
+	ASSERT_TRUE(\
+			spLoggerPrintError(NULL,"sp_logger_unit_test.c",__func__,__LINE__) == SP_LOGGER_INVAlID_ARGUMENT); \
+	ASSERT_TRUE( \
+			spLoggerPrintError("a",NULL,__func__,__LINE__) == SP_LOGGER_INVAlID_ARGUMENT); \
+	ASSERT_TRUE( \
+			spLoggerPrintError("a","b",NULL,5) == SP_LOGGER_INVAlID_ARGUMENT); \
+	ASSERT_TRUE( \
+			spLoggerPrintError("a", "b", "c", -1) == SP_LOGGER_INVAlID_ARGUMENT); }
+
+// This is a helper function which checks if two files are identical
+static bool identicalFiles(const char* fname1, const char* fname2) {
+	FILE *fp1, *fp2;
+	fp1 = fopen(fname1, "r");
+	fp2 = fopen(fname2, "r");
+	char ch1 = EOF, ch2 = EOF;
+
+	if (fp1 == NULL) {
+		return false;
+	} else if (fp2 == NULL) {
+		fclose(fp1);
+		return false;
+	} else {
+		ch1 = getc(fp1);
+		ch2 = getc(fp2);
+
+		while ((ch1 != EOF) && (ch2 != EOF) && (ch1 == ch2)) {
+			ch1 = getc(fp1);
+			ch2 = getc(fp2);
+		}
+		fclose(fp1);
+		fclose(fp2);
+	}
+	if (ch1 == ch2) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// Check edge cases for creating logger
+/*static bool loggerCreateTest() {
+ // TODO what should go here?
+ spLoggerDestroy();
+ return true;
+ }*/
+
+// Check if all functions behave correctly when Logger is not defined
+static bool loggerDefinedTest() {
+	ASSERT_TRUE(
+			spLoggerPrintError("A","sp_logger_unit_test.c",__func__,__LINE__) == SP_LOGGER_UNDIFINED);
+	ASSERT_TRUE(
+			spLoggerPrintWarning("B","sp_logger_unit_test.c",__func__,__LINE__) == SP_LOGGER_UNDIFINED);
+	ASSERT_TRUE(spLoggerPrintInfo("C") == SP_LOGGER_UNDIFINED);
+	ASSERT_TRUE(
+			spLoggerPrintDebug("D","sp_logger_unit_test.c",__func__,__LINE__) == SP_LOGGER_UNDIFINED);
+	spLoggerDestroy();
+
+	// All functions know when to return "SP_LOGGER_UNDIFINED"
+	return true;
+}
+
+static bool loggerInvalidArgumentTest() {
+	// set logger
+	ASSERT_TRUE(
+			spLoggerCreate(NULL, SP_LOGGER_ERROR_LEVEL) == SP_LOGGER_SUCCESS);
+
+	// PrintError:
+	checkArguments(spLoggerPrintError);
+	// PrintWarning:
+	checkArguments(spLoggerPrintWarning);
+	// PrintDebug:
+	checkArguments(spLoggerPrintDebug);
+	// PrintMsg:
+	ASSERT_TRUE(spLoggerPrintMsg(NULL) == SP_LOGGER_INVAlID_ARGUMENT);
+
+	spLoggerDestroy();
+	return true;
+}
+
+// Only errors should be printed
+static bool loggerErrorTest() {
+	const char* expectedFile = "loggerErrorTestExp.log";
+	const char* testFile = "loggerErrorTest.log";
+	ASSERT_TRUE(spLoggerCreate(testFile, SP_LOGGER_ERROR_LEVEL) == SP_LOGGER_SUCCESS);
+	ASSERT_TRUE(
+			spLoggerPrintError("MSGA","sp_logger_unit_test.c",__func__,__LINE__) == SP_LOGGER_SUCCESS);
+	ASSERT_TRUE(
+			spLoggerPrintWarning("MSGB","sp_logger_unit_test.c",__func__,__LINE__) == SP_LOGGER_SUCCESS);
+	ASSERT_TRUE(spLoggerPrintInfo("MSGC") == SP_LOGGER_SUCCESS);
+	ASSERT_TRUE(
+			spLoggerPrintDebug("MSGD","sp_logger_unit_test.c",__func__,__LINE__) == SP_LOGGER_SUCCESS);
+	spLoggerDestroy();
+	ASSERT_TRUE(identicalFiles(testFile, expectedFile));
+	return true;
+}
+
+// should print only warnings and errors
+static bool loggerWarningTest() {
+	const char* expectedFile = "loggerWarningTestExp.log";
+	const char* testFile = "loggerWarningTest.log";
+	ASSERT_TRUE(
+			spLoggerCreate(testFile, SP_LOGGER_WARNING_ERROR_LEVEL)
+					== SP_LOGGER_SUCCESS); // set logger
+
+// should be printed
+	ASSERT_TRUE(
+			spLoggerPrintError("MSGA","sp_logger_unit_test.c",__func__,__LINE__) == SP_LOGGER_SUCCESS);
+
+// should be printed
+	ASSERT_TRUE(
+			spLoggerPrintWarning("MSGB","sp_logger_unit_test.c",__func__,__LINE__) == SP_LOGGER_SUCCESS);
+
+// should not be printed
+	ASSERT_TRUE(spLoggerPrintInfo("MSGC") == SP_LOGGER_SUCCESS);
+
+// should not be printed
+	ASSERT_TRUE(
+			spLoggerPrintDebug("MSGD","sp_logger_unit_test.c",__func__,__LINE__) == SP_LOGGER_SUCCESS);
+
+	spLoggerDestroy();
+	ASSERT_TRUE(identicalFiles(testFile, expectedFile));
+	return true;
+}
+
+// All messages except debug should be printed in info level
+static bool loggerInfoTest() {
+	const char* expectedFile = "loggerInfoTestExp.log";
+	const char* testFile = "loggerInfoTest.log";
+	ASSERT_TRUE(
+			spLoggerCreate(testFile, SP_LOGGER_INFO_WARNING_ERROR_LEVEL)
+					== SP_LOGGER_SUCCESS);
+	ASSERT_TRUE(
+			spLoggerPrintError("MSGA","sp_logger_unit_test.c",__func__,__LINE__) == SP_LOGGER_SUCCESS);
+	ASSERT_TRUE(
+			spLoggerPrintWarning("MSGB","sp_logger_unit_test.c",__func__,__LINE__) == SP_LOGGER_SUCCESS);
+	ASSERT_TRUE(spLoggerPrintInfo("MSGC") == SP_LOGGER_SUCCESS);
+
+// This one should not be printed
+	ASSERT_TRUE(
+			spLoggerPrintDebug("MSGD","sp_logger_unit_test.c",__func__,__LINE__) == SP_LOGGER_SUCCESS);
+	spLoggerDestroy();
+	ASSERT_TRUE(identicalFiles(testFile, expectedFile));
+	return true;
+}
+
+//All messages should be printed in debug level
+static bool loggerDebugTest() {
+	const char* expectedFile = "loggerDebugTestExp.log";
+	const char* testFile = "loggerDebugTest.log";
+	ASSERT_TRUE(
+			spLoggerCreate(testFile, SP_LOGGER_DEBUG_INFO_WARNING_ERROR_LEVEL)
+					== SP_LOGGER_SUCCESS);
+	ASSERT_TRUE(
+			spLoggerPrintError("MSGA","sp_logger_unit_test.c",__func__,__LINE__) == SP_LOGGER_SUCCESS);
+	ASSERT_TRUE(
+			spLoggerPrintWarning("MSGB","sp_logger_unit_test.c",__func__,__LINE__) == SP_LOGGER_SUCCESS);
+	ASSERT_TRUE(spLoggerPrintInfo("MSGC") == SP_LOGGER_SUCCESS);
+	ASSERT_TRUE(
+			spLoggerPrintDebug("MSGD","sp_logger_unit_test.c",__func__,__LINE__) == SP_LOGGER_SUCCESS);
+	spLoggerDestroy();
+	ASSERT_TRUE(identicalFiles(testFile, expectedFile));
+	return true;
+}
+
+int main() {
+//RUN_TEST(loggerCreateTest);
+	RUN_TEST(loggerDefinedTest);
+	RUN_TEST(loggerInvalidArgumentTest);
+	RUN_TEST(loggerErrorTest);
+	RUN_TEST(loggerWarningTest);
+	RUN_TEST(loggerInfoTest);
+	RUN_TEST(loggerDebugTest);
+
+	return 0;
+}
