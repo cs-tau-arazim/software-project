d51919dfcddcb575a364887250248112c0dc65a1
galmw <Gal@Gals-MacBook-Pro.local>
2016-06-27 19:14:42 +0300

    started spconfig

---
+++ SPFinalProject/SPConfig.c
@@ -0,0 +1,49 @@
+#include "SPConfig.h"
+
+/*
+ * SPConfig.c
+ *
+ *  Created on: Jun 27, 2016
+ *      Author: Gal
+ */
+
+
+/**
+ * Creates a new system configuration struct. The configuration struct
+ * is initialized based on the configuration file given by 'filename'.
+ *
+ * @param filename - the name of the configuration file
+ * @assert msg != NULL
+ * @param msg - pointer in which the msg returned by the function is stored
+ * @return NULL in case an error occurs. Otherwise, a pointer to a struct which
+ * 		   contains all system configuration.
+ *
+ * The resulting value stored in msg is as follow:
+ * - SP_CONFIG_INVALID_ARGUMENT - if filename == NULL
+ * - SP_CONFIG_CANNOT_OPEN_FILE - if the configuration file given by filename cannot be open
+ * - SP_CONFIG_ALLOC_FAIL - if an allocation failure occurred
+ * - SP_CONFIG_INVALID_INTEGER - if a line in the config file contains invalid integer
+ * - SP_CONFIG_INVALID_STRING - if a line in the config file contains invalid string
+ * - SP_CONFIG_MISSING_DIR - if spImagesDirectory is missing
+ * - SP_CONFIG_MISSING_PREFIX - if spImagesPrefix is missing
+ * - SP_CONFIG_MISSING_SUFFIX - if spImagesSuffix is missing
+ * - SP_CONFIG_MISSING_NUM_IMAGES - if spNumOfImages is missing
+ * - SP_CONFIG_SUCCESS - in case of success
+ *
+ *
+ */
+SPConfig spConfigCreate(const char* filename, SP_CONFIG_MSG* msg) {
+	// define variables
+	FILE *configFile;
+
+	assert(msg != NULL); // assertion
+
+	configFile = fopen(filename, "r");
+	if ( configFile == NULL) { // edge case
+		(*msg) = SP_CONFIG_CANNOT_OPEN_FILE;
+		return NULL;
+	}
+
+	// now we need to read from the file
+
+}
