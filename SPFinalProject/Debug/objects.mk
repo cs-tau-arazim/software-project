91f06ca7686ea276fabf2018ef4006bd60ae2905
Tom Segal <tomsegal@localhost.localdomain>
2016-06-28 18:00:33 +0300

    added files from last ex

---
+++ SPFinalProject/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lopencv_features2d -lopencv_xfeatures2d -lopencv_flann -lopencv_cudaarithm -lopencv_core -lopencv_imgproc -lopencv_imgcodecs -lopencv_highgui
+
---
+++ SPFinalProject/Debug/subdir.mk
@@ -0,0 +1,47 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../SPImageProc.cpp \
+../main.cpp 
+
+C_SRCS += \
+../SPConfig.c \
+../SPLogger.c \
+../SPPoint.c 
+
+OBJS += \
+./SPConfig.o \
+./SPImageProc.o \
+./SPLogger.o \
+./SPPoint.o \
+./main.o 
+
+CPP_DEPS += \
+./SPImageProc.d \
+./main.d 
+
+C_DEPS += \
+./SPConfig.d \
+./SPLogger.d \
+./SPPoint.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I/usr/local/lib/opencv-3.1.0/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
