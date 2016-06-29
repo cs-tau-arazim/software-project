cf8890d68a5d8d7adec44259f59fe37d620983bd
Tom Segal <tomsegal@localhost.localdomain>
2016-06-29 15:10:12 +0300

    pull

---
+++ SPFinalProject/Debug/main_aux.d
@@ -0,0 +1,19 @@
+main_aux.d: ../main_aux.c ../main_aux.h ../SPConfig.h ../SPLogger.h \
+ ../KDTreeNode.h ../SPPoint.h ../SPBPriorityQueue.h ../SPListElement.h \
+ ../SPList.h
+
+../main_aux.h:
+
+../SPConfig.h:
+
+../SPLogger.h:
+
+../KDTreeNode.h:
+
+../SPPoint.h:
+
+../SPBPriorityQueue.h:
+
+../SPListElement.h:
+
+../SPList.h:
