f2ad327de2f59ae79f633c935a48f94270a5966e
galmw <galwiernik@gmail.com>
2016-06-29 13:26:07 +0300

    main

---
+++ SPFinalProject/main_aux.c
@@ -0,0 +1,50 @@
+/*
+ * main_aux.c
+ *
+ *  Created on: Jun 29, 2016
+ *      Author: galwiernik
+ */
+
+#include
+
+/**
+ * Helper function to print the appropriate message
+ * for each error received by spConfigCreate.
+ */
+void printErrorType(SP_CONFIG_MSG * configMsg) {
+	if ((*configMsg) == SP_CONFIG_INVALID_ARGUMENT)
+		printf("Message: No configuration file");
+	if ((*configMsg) == SP_CONFIG_CANNOT_OPEN_FILE)
+		printf("Message: Invalid configuration file");
+	if ((*configMsg) == SP_CONFIG_ALLOC_FAIL)
+		printf("Message: Memory allocation failure occurred");
+
+	if ((*configMsg) == SP_CONFIG_INVALID_INTEGER) {
+		printf("Line: ??????"); // LINE CHECK MEANING
+		printf("Message: Invalid value - constraint not met");
+	}
+
+	if ((*configMsg) == SP_CONFIG_INVALID_INTEGER
+			|| (*configMsg) == SP_CONFIG_INVALID_STRING) {
+		printf("Line: ??????"); // LINE CHECK MEANING
+		printf("Message: Invalid value - constraint not met");
+	}
+	if ((*configMsg) == SP_CONFIG_MISSING_DIR) {
+		printf("Line: ??????"); // LINE CHECK MEANING
+		printf("Message: Parameter spImagesDirectory is not set");
+	}
+	if ((*configMsg) == SP_CONFIG_MISSING_PREFIX) {
+		printf("Line: ??????"); // LINE CHECK MEANING
+		printf("Message: Parameter spImagesPrefix is not set");
+	}
+	if ((*configMsg) == SP_CONFIG_MISSING_SUFFIX) {
+		printf("Line: ??????"); // LINE CHECK MEANING
+		printf("Message: Parameter spImagesSuffix is not set");
+	}
+	if ((*configMsg) == SP_CONFIG_MISSING_NUM_IMAGES) {
+		printf("Line: ??????"); // LINE CHECK MEANING
+		printf("Message: Parameter spNumOfImages is not set");
+	}
+
+}
+
---
+++ SPFinalProject/main_aux.h
@@ -0,0 +1,18 @@
+/*
+ * main_aux.h
+ *
+ *  Created on: Jun 29, 2016
+ *      Author: galwiernik
+ */
+
+#ifndef MAIN_AUX_H_
+#define MAIN_AUX_H_
+
+/**
+ * Helper function to print the appropriate message
+ * for each error received by spConfigCreate.
+ */
+void printErrorType(SP_CONFIG_MSG * configMsg);
+
+
+#endif /* MAIN_AUX_H_ */
